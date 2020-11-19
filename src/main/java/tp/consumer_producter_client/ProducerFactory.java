3fafded48639192136a65726eaf9235f477c2807
NekoRomain <romainwangon@gmail.com>
2020-11-19 10:18:05 +0100

    Ajout projet

---
+++ src/main/java/tp/consumer_producter_client/ProducerFactory.java
@@ -0,0 +1,22 @@
+package tp.consumer_producter_client;
+
+import org.apache.kafka.clients.producer.*;
+
+import java.util.Properties;
+
+public class ProducerFactory {
+
+    public static Producer<String, String> createProducer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+
+        return new KafkaProducer<String, String>(props);
+
+    }
+
+
+
+
+}
