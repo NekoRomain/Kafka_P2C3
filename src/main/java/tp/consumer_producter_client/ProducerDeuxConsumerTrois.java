30d03f55aab4e8dea2b2e30aed755a52ad1fe2a0
NekoRomain <romainwangon@gmail.com>
2020-11-19 11:00:48 +0100

    Menu command + envoi

--- src/main/java/tp/consumer_producter_client/ProducerDeuxConsumerTrois.java
+++ src/main/java/tp/consumer_producter_client/ProducerDeuxConsumerTrois.java
@@ -32,24 +32,24 @@ public class ProducerDeuxConsumerTrois implements Runnable {
 
         boolean attendReponse = false;
         while (!Thread.interrupted()){
-//            Console console = System.console();
-//            cmd = console.readLine("Entrer votre commande: ");
-
-            System.out.print("Entrer votre commande: ");
-            cmd = scanner.nextLine();
+            do{
+                System.out.print("Entrer votre commande: ");
+                cmd = scanner.nextLine();
+            }while (!menu(cmd));
             ProducerRecord<String, String> record = new ProducerRecord<String, String>(topicProducer, cmd);
             producer.send(record, new ProducerCallBack());
             attendReponse = true;
-            System.out.println("-----------Réponse-----------");
-//            while (attendReponse){
-//                ConsumerRecords<String, String> records = consumer.poll(100);
-//                if(records != null && !records.isEmpty()){
-//                    attendReponse = false;
-//                    records.forEach(message -> {
-//                        System.out.println(message.toString());
-//                    });
-//                }
-//            }
+            System.out.println("-----------En attente de réponse-----------");
+            while (attendReponse){
+                ConsumerRecords<String, String> records = consumer.poll(100);
+                if(records != null && !records.isEmpty()){
+                    attendReponse = false;
+                    System.out.println("\n\n-----------Réponse-----------");
+                    records.forEach(message -> {
+                        System.out.println(message.toString());
+                    });
+                }
+            }
 
         }
         scanner.close();
@@ -61,6 +61,44 @@ public class ProducerDeuxConsumerTrois implements Runnable {
             if (e != null){
                 e.printStackTrace();
             }
+        }
+    }
+
+    private boolean menu(String cmd){
+        String[] cmd_split = cmd.split(" ");
+        switch (cmd_split[0]){
+            case "get_global_values":
+                return true;
+            case "get_country_values":
+                if(cmd_split[1] != null && cmd_split[1].length() == 2)
+                    return true;
+                else {
+                    System.out.println("----------ERROR------------");
+                    System.out.println("Le country code est necessaire ou il est incorecte (taille).");
+                    System.out.println("--------------------------- ");
+                }
+            case "get_confirmed_avg":
+                return true;
+            case "get_deaths_avg":
+                return true;
+            case "get_countries_deaths_percent":
+                return true;
+            case "help":
+                System.out.println("----------HELP--------------");
+                System.out.println("get_global_values : retourne les valeurs globales clés Global du fichier json.");
+                System.out.println("get_country_values v_pays : retourne les valeurs du pays demandé ou v_pays est une chaine de caractère du pays demandé (CountryCode, lenght=2).");
+                System.out.println("get_confirmed_avg : retourne une moyenne des cas confirmés sum(pays)/nb(pays)");
+                System.out.println("get_deaths_avg : retourne une moyenne des Décès sum(pays)/nb(pays)");
+                System.out.println("get_countries_deaths_percent : retourne le pourcentage de Décès par rapport aux cas confirmés");
+                System.out.println("help : affiche la liste des commandes et une explication.");
+                System.out.println("--------------------------- ");
+                return false;
+            default:
+                System.out.println("----------ERROR------------");
+                System.out.println("Commande inconnue.\nTaper help pour plus d'information.");
+                System.out.println("--------------------------- ");
+                return false;
+
         }
     }
 
